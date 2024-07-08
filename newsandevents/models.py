cb26b883eb6bba9abdfb24d7e0b2cbf9344beb97
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-07-08 16:11:44 +0300

    add newsandevents app and models

---
+++ newsandevents/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class News(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='news_images/')
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='event_images/')
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.title
