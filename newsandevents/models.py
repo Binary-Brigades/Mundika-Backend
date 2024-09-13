af27558c095b1a3cc731c43d0fead8e80626180d
Mikiejoe <sitb01-02668201@student.mmust.ac.ke>
2024-09-13 10:21:58 +0300

    add club and societies models

--- newsandevents/models.py
+++ newsandevents/models.py
@@ -7,8 +7,7 @@ class News(models.Model):
     date = models.DateField()
     
     def __str__(self):
-        return self.title
-    
+        return self.title   
     
 class Event(models.Model):
     title = models.CharField(max_length=50)
